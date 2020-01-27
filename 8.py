a46318a185c4aef186683d7a53d1f64129f1e308
IanLi <jasmine_chieh_li@outlook.com>
2020-01-27 22:50:37 +0800

    fix 8.py - all sorted issue

--- 8.py
+++ 8.py
@@ -8,10 +8,13 @@ class Solution:
         i = 0
         j = len(nums) - 1
 
+        if nums[i] < nums[j]:
+            return nums[i]
+
         pivot = nums[0]
         
         while j - i > 1:
-            mid = int(i + (j - i) / 2)
+            mid = int((i + j) / 2)
             if nums[mid] >= pivot:
                 i = mid
             else:
@@ -22,6 +25,6 @@ class Solution:
 if __name__ == '__main__':
     # demo = [5,6,7,8,1,2,3,4]
     # demo = [7,8,2,3,4,5,6,7]
-    demo = [2,3,4,5,5,6,7,1]
+    demo = [2,3,4,5,5,6,7]
     solution = Solution()
     print(solution.find_min(demo))
